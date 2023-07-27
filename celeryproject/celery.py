ea7fe27115e9348149343847060e2b9ca5e91d13
giftedmatrix <ebutejonah386@gmail.com>
2023-07-27 17:02:39 +0100

    first commit

---
+++ celeryproject/celery.py
@@ -0,0 +1,15 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryproject.settings')
+app = Celery("celeryproject")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+#app.conf.task_routes = {'newapp.tasks.task1':{'queue':'queue1'}, 'newapp.tasks.task2':{'queue':'queue2'}}
+
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'sep':':',
+    'queue_order_strategy':'priority',
+}
+
+app.autodiscover_tasks()
